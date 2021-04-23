b131eb9600d0d2f0ba6b8e097563bcc641ddbb3c
davidmedero <david.gregory.medero@gmail.com>
2021-04-23 17:45:11 -0400

    david finished

--- src/App.js
+++ src/App.js
@@ -4,23 +4,19 @@ import Home from './Components/Home';
 import Allbeers from './Components/Allbeers';
 import Randombeer from './Components/Randombeer';
 import Newbeer from './Components/Newbeer';
-
-
-
+import BeerDetails from './Components/BeerDetails';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
-
-
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/Allbeers" component={Allbeers} />
         <Route exact path="/Randombeer" component={Randombeer} />
         <Route exact path="/Newbeer" component={Newbeer} />
+        <Route exact path="/beer/:poopy" component={BeerDetails} />
       </Switch>
     </div>
   );
--- src/Components/Allbeers.jsx
+++ src/Components/Allbeers.jsx
@@ -1,57 +1,39 @@
-
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
 
-//https://ih-beers-api2.herokuapp.com/beers 
-
+//https://ih-beers-api2.herokuapp.com/beers
 
 const Allbeers = () => {
-    const [beer, setBeer]= useState([])
+  const [beer, setBeer] = useState([]);
 
-    useEffect(()=> {
-
-        axios.get("https://ih-beers-api2.herokuapp.com/beers ").then(res =>{
-            setBeer(res.data)
-    })
-
-
-    },[])
-const showBeers=() =>{
-    return beer.map((beer,i)=>{
-        return(
-            <div>
-
-                <div>
-                <img src = {beer.image_url}/>
-
-
-                </div>
-                <div>
-                  
-                </div>
-            </div>
-
-
-        )
-
-
-
-    })
-
-
-}
-
-
-
-
-    return (
+  useEffect(() => {
+    axios.get('https://ih-beers-api2.herokuapp.com/beers ').then((res) => {
+      setBeer(res.data);
+    });
+  }, []);
+  const showBeers = () => {
+    return beer.map((beer, i) => {
+      return (
         <div>
-            Cody is cool!!
+          <div>
+            <Link to={`/beer/${beer._id}`}>
+              <img src={beer.image_url} />
+            </Link>
+          </div>
+          <div></div>
+        </div>
+      );
+    });
+  };
 
-            {showBeers()}
-gi        </div>
-    );
+  return (
+    <div>
+      Cody is cool!!
+      {showBeers()}
+      gi{' '}
+    </div>
+  );
 };
 
 export default Allbeers;
