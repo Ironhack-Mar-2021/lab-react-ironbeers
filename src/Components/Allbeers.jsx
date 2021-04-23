bae214f6bc1568da09f446306af2aa5a11bcdef7
Dani Syku <danisyku@gmail.com>
2021-04-23 16:58:43 -0400

    next david

--- src/Components/Allbeers.jsx
+++ src/Components/Allbeers.jsx
@@ -1,9 +1,55 @@
-import React from 'react';
+
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
+
+//https://ih-beers-api2.herokuapp.com/beers 
+
 
 const Allbeers = () => {
+    const [beer, setBeer]= useState([])
+
+    useEffect(()=> {
+
+        axios.get("https://ih-beers-api2.herokuapp.com/beers ").then(res =>{
+            setBeer(res.data)
+    })
+
+
+    },[])
+const showBeers=() =>{
+    return beer.map((beer,i)=>{
+        return(
+            <div>
+
+                <div>
+                <img src = {beer.image_url}/>
+
+
+                </div>
+                <div>
+                  
+                </div>
+            </div>
+
+
+        )
+
+
+
+    })
+
+
+}
+
+
+
+
     return (
         <div>
-            Cody is cool
+            Cody is cool!!
+
+            {showBeers()}
         </div>
     );
 };
